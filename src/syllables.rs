9d2d556cb35e4ffdb17319bd6f4c658d5586bd54
Satyam Singh <trueleo@protonmail.com>
2021-09-02 17:57:40 +0530

    remove redundant lifetime specifier

--- src/syllables.rs
+++ src/syllables.rs
@@ -9,7 +9,7 @@ pub enum Syllable {
     Alphabet,
 }
 
-pub const INITIALS: [&'static str; 121] = [
+pub const INITIALS: [&str; 121] = [
     "bl", "br", "cl", "cr", "dr", "fl", "fr", "gl", "gr", "pl", "pr", "sk", "sl", "sm", "sn", "sp",
     "st", "str", "sw", "tr", "ch", "sh", "m", "c", "b", "r", "d", "h", "s", "p", "l", "g", "f", "w",
     "t", "k", "n", "v", "st", "pr", "j", "br", "ch", "gr", "sh", "tr", "cr", "fr", "z", "sp", "wh",
@@ -19,11 +19,11 @@ pub const INITIALS: [&'static str; 121] = [
     "mcd", "hydr", "kh", "ts", "mcl", "by", "spl", "dw", "pf", "bh", "ryn", "typ", "cz", "sr",
     "fry", "gn", "hr", "hy", "syn", "sz", "kw", "dyn", "ll", "pc", "cyr"
   ];
-pub const VOWELS: [&'static str; 46] = ["a", "e", "i", "o", "u", "oo",
+pub const VOWELS: [&str; 46] = ["a", "e", "i", "o", "u", "oo",
 "e", "a", "i", "o", "u", "ia", "ie", "ee", "io", "au", "ea", "ou", "ai", "ue", "ei", "eau", "eu", "oe", "ae", "eo",
   "oa", "oo", "ao", "ua", "oi", "ui", "aa", "ieu", "uo", "oia", "aue", "iu", "aia", "iou", "ii", "aio", "uie", "eia","uia", "iao"
 ];
-pub const FINALS: [&'static str; 113] = [
+pub const FINALS: [&str; 113] = [
     "ct", "ft", "mp", "nd", "ng", "nk", "nt", "pt", "sk", "sp", "ss", "st", "ch", "sh",
     "s", "n", "r", "d", "ng", "l", "y", "rs", "ns", "t", "m", "ll", "nt", "c", "ck", "st", "k", "ss", "sts", "rd", "nd",
   "ry", "rt", "w", "lly", "tt", "ch", "ts", "ty", "p", "ls", "ld", "nts", "x", "rg", "sh", "ly", "th", "ff", "g", "rn", "ngs",
